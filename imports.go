82b3bb50cbb891751de00c0caff6922dd7849972
Mit Naria <mit.n@media.net>
2018-12-13 19:15:25 +0530

    Playing with Go's factored import statement.
    1. Notice the difference between Println and Printf.

---
+++ imports.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Notice the difference between Println and Prinf.", math.Sqrt(5))
+	fmt.Printf("You have %g problems.", math.Sqrt(5))
+	fmt.Println("END")
+}
